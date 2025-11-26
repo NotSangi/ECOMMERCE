a25cf3bb00be7aa4526dc3993d3b20571ce364b3
Santiago <gironlozano1975@gmail.com>
2025-11-25 21:22:11 -0500

    Checkout and modifications to Login and Cart functions

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 from .forms import RegistrationForm
 from .models import Account
+from django.db import transaction
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
+import requests
 
-# EMAIL
+# EMAIL IMPORTS
 from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
@@ -12,9 +14,11 @@ from django.contrib.auth.tokens import default_token_generator
 from django.utils.encoding import force_bytes
 from django.core.mail import EmailMessage
 
+#ASSING CART TO USER 
+from carts.views import _cart_id
+from carts.models import Cart, CartItem
 
-
-# Create your views here.
+# ----- START AUTHENTICATION ----- #
 def register(request):
     form = RegistrationForm()
     if request.method == 'POST':
@@ -62,18 +66,67 @@ def register(request):
 
 def login(request):
     if request.method == 'POST':
-        email = request.POST['email']
-        password = request.POST['password']
-        
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
         user = auth.authenticate(email=email, password=password)
-        
+
         if user is not None:
+            # Add anonymous cart to user's cart
+            try:
+                cart = Cart.objects.get(cart_id=_cart_id(request))
+            except Cart.DoesNotExist:
+                cart = None
+
+            if cart:
+                with transaction.atomic():
+                    # Bring in anonymous items and user items
+                    anon_items = CartItem.objects.filter(cart=cart)
+                    user_items = CartItem.objects.filter(user=user)
+
+                    # We built a map for quick searches in the user's shopping cart
+                    # key = (product_id, frozenset(variation_ids))
+                    user_map = {}
+                    for ui in user_items:
+                        var_ids = frozenset(ui.variations.values_list('id', flat=True))
+                        user_map[(ui.product_id, var_ids)] = ui
+
+                    # We go through each anonymous item and merge/transfer it
+                    for a in anon_items:
+                        a_var_ids = frozenset(a.variations.values_list('id', flat=True))
+                        key = (a.product_id, a_var_ids)
+
+                        if key in user_map:
+                            # An identical item already exists in the user's cart -> add quantities
+                            u_item = user_map[key]
+                            u_item.quantity = u_item.quantity + a.quantity
+                            u_item.save()
+                            # We deleted the anonymous item because it has already been merged
+                            a.delete()
+                        else:
+                            # Does not exist -> assign the anonymous item to the user (transfer)
+                            a.user = user
+                            a.save()
+                            # Add to map to avoid future duplications in the same loop
+                            user_map[key] = a
+
             auth.login(request, user)
-            return redirect('home')
+            messages.success(request, 'Has iniciado sesión correctamente.')
+            url = request.META.get('HTTP_REFERER')
+            try:
+                query = requests.utils.urlparse(url).query
+                params = dict(x.split('=') for x in query.split('&') if x)
+                if 'next' in params:
+                    return redirect(params['next'])
+            except Exception:
+                pass
+
+            return redirect('dashboard')
+
         else:
             messages.error(request, 'Incorrect Credentials')
             return redirect('login')
-    
+
     return render(request, 'accounts/login.html')
 
 @login_required(login_url='login')
@@ -82,6 +135,9 @@ def logout(request):
     messages.success(request, 'You have logged out')
     return redirect('login')
 
+# ----- END AUTHENTICATION ----- #
+
+# ----- ACTIVATE ACCOUNT VIA EMAIL ----- #
 def activate(request, uidb64, token):
     try:
         uid = urlsafe_base64_decode(uidb64).decode()
@@ -97,3 +153,77 @@ def activate(request, uidb64, token):
     else:
         messages.error(request, 'Invalid Activation')
         return redirect('register')
+    
+# --------------------------------------- #
+
+# ----- USER DASHBOARD ----- #
+@login_required(login_url='login')
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
+
+# -------------------------- #
+
+# ----- START RESET PASSWORD ----- #
+
+def forgot_password(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        if Account.objects.filter(email=email).exists():
+            user = Account.objects.get(email__exact=email)
+            
+            current_site = get_current_site(request)
+            mail_subject = 'Reset Password'
+            body = render_to_string('accounts/reset_password_email.html', {
+                'user': user,
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+                
+            to_email = email
+            send_email = EmailMessage(mail_subject, body, to=[to_email])
+            send_email.send()
+            
+            messages.success(request, 'We have sent you an email to reset your password')
+            return redirect('login')
+        
+        else:
+            messages.error(request, "The account doesnt exists")
+            return redirect('forgot_password')
+            
+    return render(request, 'accounts/forgot_password.html')
+
+def reset_password_validate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user = None
+        
+    if user is not None and default_token_generator.check_token(user, token):
+        request.session['uid'] = uid
+        messages.success(request, 'Please reset your password')
+        return redirect('reset_password')
+    else:
+        messages.error(request, 'Invalid Link')
+        return redirect('login')
+
+def reset_password(request):
+    if request.method == 'POST':
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        
+        if password == confirm_password:
+            uid = request.session.get('uid')
+            user = Account.objects.get(pk=uid)
+            user.set_password(password)
+            user.save()
+            messages.success(request, 'Password has been successfully reset')
+            return redirect('login')
+        else:
+            messages.error(request, 'Confirmation Password doesnt match')
+            return redirect('reset_password')
+    else:
+        return render(request, 'accounts/reset_password.html')
+    
+# ----- END RESET PASSWORD ----- #
--- carts/views.py
+++ carts/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render, redirect, get_object_or_404
 from store.models import Product, Variation
 from .models import Cart, CartItem
 from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+# ----- START CART LOGIC ----- #
 def _cart_id(request):
     cart = request.session.session_key
     if not cart:
@@ -24,18 +27,24 @@ def add_cart(request, product_id):
             except:
                 pass
     
-    try:
-        cart = Cart.objects.get(cart_id=_cart_id(request))
-    except Cart.DoesNotExist:
-        cart = Cart.objects.create(
-            cart_id = _cart_id(request)
-        )
-    cart.save()
-    
-    is_cart_item_exists = CartItem.objects.filter(product=product, cart=cart).exists()
+    if request.user.is_authenticated:
+        is_cart_item_exists = CartItem.objects.filter(product=product, user=request.user).exists()
+    else:
+        try:
+            cart = Cart.objects.get(cart_id=_cart_id(request))
+        except Cart.DoesNotExist:
+            cart = Cart.objects.create(
+                cart_id = _cart_id(request)
+            )
+        cart.save()
+        
+        is_cart_item_exists = CartItem.objects.filter(product=product, cart=cart).exists()
     
     if is_cart_item_exists:
-        cart_item = CartItem.objects.filter(product=product, cart=cart)
+        if request.user.is_authenticated:
+            cart_item = CartItem.objects.filter(product=product, user=request.user)
+        else:
+            cart_item = CartItem.objects.filter(product=product, cart=cart)
         ex_var_list = []
         id = []
         
@@ -51,17 +60,29 @@ def add_cart(request, product_id):
             item.quantity += 1
             item.save()
         else:
-            item = CartItem.objects.create(product=product, quantity=1, cart=cart)
+            if request.user.is_authenticated:
+                item = CartItem.objects.create(product=product, quantity=1, user=request.user)
+            else:
+                item = CartItem.objects.create(product=product, quantity=1, cart=cart)
+                
             if len(product_variation) > 0:
                 item.variations.clear()
                 item.variations.add(*product_variation)
             item.save()
     else:
-        cart_item = CartItem.objects.create(
-            product = product,
-            quantity = 1,
-            cart = cart,
-        )
+        if request.user.is_authenticated:   
+            cart_item = CartItem.objects.create(
+                product = product,
+                quantity = 1,
+                user=request.user,
+            )
+        else:
+            cart_item = CartItem.objects.create(
+                product = product,
+                quantity = 1,
+                cart = cart,
+            )
+        
         if len(product_variation) > 0:
             cart_item.variations.clear()
             cart_item.variations.add(*product_variation)
@@ -70,11 +91,14 @@ def add_cart(request, product_id):
     return redirect('cart')
 
 def remove_cart(request, product_id, cart_item_id):
-    cart = Cart.objects.get(cart_id=_cart_id(request))
     product = get_object_or_404(Product, id=product_id)
     
     try:
-        cart_item = CartItem.objects.get(cart=cart, product=product, id=cart_item_id)
+        if request.user.is_authenticated:
+            cart_item = CartItem.objects.get(user=request.user, product=product, id=cart_item_id)
+        else:
+            cart = Cart.objects.get(cart_id=_cart_id(request))
+            cart_item = CartItem.objects.get(cart=cart, product=product, id=cart_item_id)
         if cart_item.quantity > 1:
             cart_item.quantity -= 1
             cart_item.save()
@@ -86,9 +110,13 @@ def remove_cart(request, product_id, cart_item_id):
     return redirect('cart')
 
 def remove_cart_item(request, product_id, cart_item_id):
-    cart = Cart.objects.get(cart_id=_cart_id(request))
     product = get_object_or_404(Product, id=product_id)
-    cart_item = CartItem.objects.get(cart=cart, product=product, id=cart_item_id)
+    
+    if request.user.is_authenticated:
+        cart_item = CartItem.objects.get(user=request.user, product=product, id=cart_item_id)
+    else:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_item = CartItem.objects.get(cart=cart, product=product, id=cart_item_id)
     
     cart_item.delete()
     
@@ -99,8 +127,41 @@ def cart(request, total=0, quantity=0, cart_items=None):
     tax=0
     grand_total=0
     try:
-        cart = Cart.objects.get(cart_id=_cart_id(request))
-        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        if request.user.is_authenticated:
+            cart_items = CartItem.objects.filter(user=request.user, is_active=True)
+        else: 
+            cart = Cart.objects.get(cart_id=_cart_id(request))
+            cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+            
+        for cart_item in cart_items:
+            total += cart_item.product.price * cart_item.quantity
+            quantity += cart_item.quantity
+        tax = round((0.02*total), 2)
+        grand_total = total + tax
+    except ObjectDoesNotExist:
+        pass
+    
+    context = {
+        'total': total,
+        'quantity': quantity,
+        'cart_items': cart_items,
+        'tax': tax,
+        'grand_total': grand_total
+    }
+    
+    return render(request, 'store/cart.html', context)
+
+# ----- END CART LOGIC ----- #
+@login_required(login_url='login')
+def checkout(request, total=0, quantity=0, cart_items=None):
+    tax=0
+    grand_total=0
+    try:
+        if request.user.is_authenticated:
+            cart_items = CartItem.objects.filter(user=request.user, is_active=True)
+        else:
+            cart = Cart.objects.get(cart_id=_cart_id(request))
+            cart_items = CartItem.objects.filter(cart=cart, is_active=True)
         for cart_item in cart_items:
             total += cart_item.product.price * cart_item.quantity
             quantity += cart_item.quantity
@@ -117,4 +178,4 @@ def cart(request, total=0, quantity=0, cart_items=None):
         'grand_total': grand_total
     }
     
-    return render(request, 'store/cart.html', context)
+    return render(request, 'store/checkout.html', context)
